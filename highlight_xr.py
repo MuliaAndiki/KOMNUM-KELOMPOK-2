8da009a2e96f162cd95e0a5a06a082e414be5274
Lingzhi09 <rezzaramadhana09@gmail.com>
2024-10-21 16:33:08 +0700

    Delete

--- highlight_xr.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-# Fungsi untuk menampilkan tabel dengan highlight pada baris terakhir
-def show_table_with_highlight(df):
-    last_index = df.index[-1]
-    table_html = df.style.apply(
-        lambda x: ['background-color: green' if x.name == last_index else '' for i in x], axis=1
-    ).render()
-    st.markdown(table_html, unsafe_allow_html=True)
