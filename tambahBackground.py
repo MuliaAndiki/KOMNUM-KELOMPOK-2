9bab3653313c34cd4994b89a59158e3b353d3d56
Lingzhi09 <rezzaramadhana09@gmail.com>
2024-10-21 16:26:18 +0700

    Delete

--- tambahBackground.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-# Membaca gambar latar belakang dari file lokal
-def load_background_image(image_path):
-    with open(image_path, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()  # Encode ke base64
